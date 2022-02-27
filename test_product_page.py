1c40eb9b3b9509f83aa4d80d14ad96e4aee34861
Yegorych <egorka_wf@mail.ru>
2022-02-27 18:29:06 +0300

    another one empty commit

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,6 @@
 import pytest
-
 from pages.login_page import LoginPage
 from pages.product_page import ProductPage
-
 #
 # @pytest.mark.parametrize('partOfLink',['0','1','2','3','4','5','6',pytest.param('7', marks=pytest.mark.xfail),'8','9',])
 # def test_guest_can_add_product_to_basket(browser):
